911c2ab71d958ac09a5156f86e24f2dba53c0454
ChemsCode <saidiceb@gmail.com>
2024-11-21 19:12:28 -0500

    modified tracklist to be tbale

--- ASSIGNMENT3/Function2/frontend/src/components/TracksList.js
+++ ASSIGNMENT3/Function2/frontend/src/components/TracksList.js
@@ -4,13 +4,24 @@ const TracksList = ({ tracks }) => {
   return (
     <div>
       <h3>Tracks</h3>
-      <ul>
-        {tracks.map((track) => (
-          <li key={track.id}>
-            {track.track_number}. {track.title} ({track.duration})
-          </li>
-        ))}
-      </ul>
+      <table border="1" style={{ borderCollapse: 'collapse', width: '100%' }}>
+        <thead>
+          <tr>
+            <th>Track Number</th>
+            <th>Title</th>
+            <th>Duration</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tracks.map((track) => (
+            <tr key={track.id}>
+              <td>{track.track_number}</td>
+              <td>{track.title}</td>
+              <td>{track.duration}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
